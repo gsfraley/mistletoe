f5f45c5612723b79701f95319fbe333e6b632ba0
Greg Fraley <gsfraley@gmail.com>
2023-12-25 22:21:32 -0500

    Cleanup of the main crate

--- src/bin/mistctl.rs
+++ src/bin/mistctl.rs
@@ -2,10 +2,10 @@ use std::fs;
 use std::path::PathBuf;
 
 use clap::{Command, value_parser, arg};
-use mistletoe::module::MistHuskModule;
+use mistletoe::husk::MistHuskModule;
 use mistletoe_api::v0_1::MistHuskInput;
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let matches = Command::new(env!("CARGO_CRATE_NAME"))
         .about("Next-level Kubernetes package manager")
         .subcommand(
@@ -22,27 +22,29 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("generate") {
         let module_path = matches.get_one::<PathBuf>("module").unwrap();
-        let mut module = MistHuskModule::from_file(&module_path).unwrap();
+        let mut module = MistHuskModule::from_file(&module_path)?;
 
         let input_file_yaml = if let Some(input_file) = matches.get_one::<PathBuf>("inputfile") {
-            let input_file_string = String::from_utf8(fs::read(input_file).unwrap()).unwrap();
-            serde_yaml::from_str::<serde_yaml::Mapping>(&input_file_string).unwrap()
+            let input_file_string = String::from_utf8(fs::read(input_file)?)?;
+            serde_yaml::from_str::<serde_yaml::Mapping>(&input_file_string)?
         } else {
-            serde_yaml::from_str("{}").unwrap()
+            serde_yaml::from_str("{}")?
         };
 
         let input_sets_yaml = if let Some(input_sets) = matches.get_one::<String>("set") {
-            serde_yaml::from_str::<serde_yaml::Mapping>(&format!("{{{input_sets}}}")).unwrap()
+            serde_yaml::from_str::<serde_yaml::Mapping>(&format!("{{{input_sets}}}"))?
         } else {
-            serde_yaml::from_str("{}").unwrap()
+            serde_yaml::from_str("{}")?
         };
 
         let mut input_mapping = serde_yaml::Mapping::new();
         input_file_yaml.into_iter().for_each(|(key, value)| { input_mapping.insert(key, value); });
         input_sets_yaml.into_iter().for_each(|(key, value)| { input_mapping.insert(key, value); });
 
-        let input = serde_yaml::to_string(&MistHuskInput { data: input_mapping }).unwrap();
+        let input = serde_yaml::to_string(&MistHuskInput { data: input_mapping })?;
 
-        println!("{}", module.generate(&input).unwrap());
+        println!("{}", module.generate(&input)?);
     }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,1 @@
-pub mod module;
+pub mod husk;
