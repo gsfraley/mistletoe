a3ee5072a82f76e1739f8a92e1ceb73c5f509454
Greg Fraley <gsfraley@gmail.com>
2023-12-30 14:45:02 -0500

    Change example to namespace-example

---
+++ examples/namespace-example/src/lib.rs
@@ -0,0 +1,32 @@
+use mistletoe_api::v1alpha1::{MistResult, MistOutput};
+use mistletoe_bind::mistletoe_headers;
+
+use indoc::formatdoc;
+use serde::Deserialize;
+
+mistletoe_headers! {"
+  name: namespace-example
+  labels:
+    mistletoe.dev/group: mistletoe-examples
+"}
+
+#[derive(Deserialize)]
+pub struct Inputs {
+    name: String,
+    namespace: String,
+}
+
+pub fn generate(inputs: Inputs) -> MistResult {
+    let name = inputs.name;
+    let namespace = inputs.namespace;
+
+    let output = MistOutput::new()
+        .with_file("namespace.yaml".to_string(), formatdoc!("
+            apiVersion: v1
+            kind: Namespace
+            metadata:
+              name: {name}
+        "));
+
+    Ok(output)
+}
